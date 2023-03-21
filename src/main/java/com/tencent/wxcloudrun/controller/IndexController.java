2b1cf9fcb0c070cb33ce3c0ba7f8b1728a641e20
VincentCorleone <707119577@qq.com>
2023-03-21 20:42:57 +0800

    add import

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.tencent.wxcloudrun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * index控制器
